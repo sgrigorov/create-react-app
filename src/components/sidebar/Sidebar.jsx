17a40eeae10026105458534ff95b2f33ea9fc711
Stan Grigorov <user.name@gmail.com>
2024-03-11 05:28:48 -0600

    My commit message

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,9 +4,15 @@ import "./Sidebar.css";
 function Sidebar () {
     return (
         <section className="Sidebar">
+          <section>
             Home Page
+          </section>
+          <section>
             About me
+          </section>
+          <section>
             Contact me
+          </section>
         </section>
     )
 }
