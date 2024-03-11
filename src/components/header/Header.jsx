6f170e3adb0a9817c8c89bfb8d4a76c08111ab0d
Stan Grigorov <user.name@gmail.com>
2024-03-11 12:32:15 -0600

    My commit message

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,7 @@ import "./Header.css";
 function Header () {
     return (
         <section className="Header">
-            <hr />
             Welcome to Stan's Web Site
-            <hr />
         </section>
     )
 }
