bafb79f7fb1d333609c131d1953cccf370e51bc4
Stan Grigorov <user.name@gmail.com>
2024-03-11 03:23:59 -0600

    My commit message

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Header () {
     return (
         <section>
+            <hr className="header-top__seperator" />
             Stan's Header
+            <hr className="header-top__seperator" />
         </section>
     )
 }
