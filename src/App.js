9e0253faa57d65d2e63398ca1d2abeed4dd63390
Stan Grigorov <user.name@gmail.com>
2024-03-11 03:29:42 -0600

    My commit message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components'
+import { Header, Footer } from './components'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer () {
+    return (
+        <section>
+            <hr className="footer-top__separator" />
+            Stan's Footer
+            <hr className="footer-top__separator" />
+        </section>
+    )
+}
+
+export default Footer;
